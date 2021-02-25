4a23246f01f141e2f1ba76ea476946023b7cf3f0
kitatas <315kitatas10703@gmail.com>
2021-02-26 01:04:38 +0900

    fix: ボタンアニメーションの修正

--- Assets/Kai/Scripts/Common/View/Button/ButtonAnimator.cs
+++ Assets/Kai/Scripts/Common/View/Button/ButtonAnimator.cs
@@ -7,20 +7,22 @@ namespace Common.View.Button
     public sealed class ButtonAnimator : MonoBehaviour
     {
         private UnityEngine.UI.Button _button;
+        private Vector3 _currentScale;
         private readonly float _rate = 0.85f;
 
         private void Awake()
         {
             _button = GetComponent<UnityEngine.UI.Button>();
+            _currentScale = transform.localScale;
         }
 
         public void Play()
         {
             DOTween.Sequence()
                 .Append(_button.image.rectTransform
-                    .DOScale(Vector3.one * _rate, Const.UI_ANIMATION_TIME))
+                    .DOScale(_currentScale * _rate, Const.UI_ANIMATION_TIME))
                 .Append(_button.image.rectTransform
-                    .DOScale(Vector3.one, Const.UI_ANIMATION_TIME));
+                    .DOScale(_currentScale, Const.UI_ANIMATION_TIME));
         }
     }
 }
