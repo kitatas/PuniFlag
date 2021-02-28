f81f1c56cd44802c1dd108458ef0fa9423e0ad00
kitatas <315kitatas10703@gmail.com>
2021-02-28 15:00:14 +0900

    add: 音量調節できるように

---
+++ Assets/Kai/Scripts/Title/View/VolumeView.cs
@@ -0,0 +1,49 @@
+using Common.Sound;
+using Common.Sound.BGM;
+using Common.Sound.SE;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+
+namespace Title.View
+{
+    public sealed class VolumeView : MonoBehaviour
+    {
+        [SerializeField] private Slider bgmSlider = default;
+        [SerializeField] private Slider seSlider = default;
+
+        [Inject]
+        private void Construct(BgmController bgmController, SeController seController)
+        {
+            seSlider
+                .OnPointerUpAsObservable()
+                .Subscribe(_ => seController.PlaySe(SeType.Decision))
+                .AddTo(seSlider);
+
+            SetVolume(bgmController, seController);
+
+            UpdateVolume(bgmController, seController);
+        }
+
+        private void SetVolume(IVolumeUseCase bgm, IVolumeUseCase se)
+        {
+            bgmSlider.value = bgm.GetVolume();
+            seSlider.value = se.GetVolume();
+        }
+
+        private void UpdateVolume(IVolumeUseCase bgm, IVolumeUseCase se)
+        {
+            bgmSlider
+                .OnValueChangedAsObservable()
+                .Subscribe(bgm.SetVolume)
+                .AddTo(bgmSlider);
+
+            seSlider
+                .OnValueChangedAsObservable()
+                .Subscribe(se.SetVolume)
+                .AddTo(seSlider);
+        }
+    }
+}
