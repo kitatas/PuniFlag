389759134df984d05a72498d41c4e381ca6ef4e3
kitatas <315kitatas10703@gmail.com>
2021-03-02 00:31:43 +0900

    fix: LoadButtonの修正

--- Assets/Kai/Scripts/Common/Transition/LoadButton.cs
+++ Assets/Kai/Scripts/Common/Transition/LoadButton.cs
@@ -1,6 +1,4 @@
-using System;
 using Common.View.Button;
-using Game.Stage.Level;
 using UniRx;
 using UnityEngine;
 using UnityEngine.UI;
@@ -9,28 +7,19 @@ using Zenject;
 namespace Common.Transition
 {
     [RequireComponent(typeof(Button))]
-    [RequireComponent(typeof(ButtonActivator))]
     [RequireComponent(typeof(ButtonAnimator))]
     public sealed class LoadButton : MonoBehaviour
     {
         [SerializeField] private SceneName sceneName = default;
         [SerializeField] private LoadType loadType = default;
-        [SerializeField] private bool isLoad = true;
 
-        private readonly Subject<Unit> _subject = new Subject<Unit>();
-        public IObservable<Unit> onPush => _subject;
-
-        private LevelModel _levelModel;
         private SceneLoader _sceneLoader;
-        private ButtonActivator _buttonActivator;
         private ButtonAnimator _buttonAnimator;
 
         [Inject]
-        private void Construct(LevelModel levelModel, SceneLoader sceneLoader)
+        private void Construct(SceneLoader sceneLoader)
         {
-            _levelModel = levelModel;
             _sceneLoader = sceneLoader;
-            _buttonActivator = GetComponent<ButtonActivator>();
             _buttonAnimator = GetComponent<ButtonAnimator>();
         }
 
@@ -40,51 +29,10 @@ namespace Common.Transition
                 .OnClickAsObservable()
                 .Subscribe(_ =>
                 {
-                    _subject.OnNext(Unit.Default);
-                    if (isLoad)
-                    {
-                        Push();
-                    }
+                    _sceneLoader.LoadScene(sceneName, loadType);
+                    _buttonAnimator.Play();
                 })
                 .AddTo(this);
-        }
-
-        public void Push()
-        {
-            LoadScene(loadType);
-            _buttonAnimator.Play();
-        }
-
-        public void LoadScene(LoadType type)
-        {
-            switch (type)
-            {
-                case LoadType.Direct:
-                    _sceneLoader.LoadScene(sceneName);
-                    break;
-                case LoadType.Next:
-                    _levelModel.LevelUp();
-                    var level = _levelModel.GetLevel();
-                    if (level < Const.STAGE_COUNT)
-                    {
-                        _sceneLoader.LoadScene(sceneName, level);
-                    }
-                    else
-                    {
-                        _sceneLoader.LoadResult();
-                    }
-                    break;
-                case LoadType.Reload:
-                    _sceneLoader.LoadScene(sceneName, _levelModel.GetLevel());
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException();
-            }
-        }
-
-        public void Activate(bool value)
-        {
-            _buttonActivator.Activate(value);
         }
     }
 }
