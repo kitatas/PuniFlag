d5eef881845b855fe90d539e437156c195de6cf9
kitatas <315kitatas10703@gmail.com>
2021-12-19 01:54:12 +0900

    add: 名称に背景を追加

--- Assets/Kai/Scripts/Title/Presentation/View/Screen/TitleScreenView.cs
+++ Assets/Kai/Scripts/Title/Presentation/View/Screen/TitleScreenView.cs
@@ -7,15 +7,18 @@ namespace Kai.Title.Presentation.View.Screen
 {
     public sealed class TitleScreenView : MonoBehaviour
     {
-        [SerializeField] private TextMeshProUGUI subTitle = default;
         [SerializeField] private TextMeshProUGUI puni = default;
         [SerializeField] private TextMeshProUGUI flag = default;
+        [SerializeField] private RectTransform puniBackground = default;
+        [SerializeField] private RectTransform flagBackground = default;
 
         public void Show(TitleScreen titleScreen)
         {
-            subTitle.SetTextData(titleScreen.subTitle);
             puni.SetTextData(titleScreen.puni);
             flag.SetTextData(titleScreen.flag);
+
+            puniBackground.sizeDelta = new Vector2(puni.preferredWidth + 0.2f, puni.preferredHeight + 0.1f);
+            flagBackground.sizeDelta = new Vector2(flag.preferredWidth + 0.2f, flag.preferredHeight + 0.1f);
         }
     }
 }
