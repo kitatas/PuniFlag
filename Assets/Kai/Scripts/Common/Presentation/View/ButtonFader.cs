030c4a17e2581d431387f758b52b3b4dfb2c17f1
kitatas <315kitatas10703@gmail.com>
2021-12-31 18:09:27 +0900

    fix: 不要な機能の削除

--- Assets/Kai/Scripts/Common/Presentation/View/ButtonFader.cs
+++ Assets/Kai/Scripts/Common/Presentation/View/ButtonFader.cs
@@ -7,14 +7,11 @@ namespace Kai.Common.Presentation.View
     public sealed class ButtonFader : MonoBehaviour
     {
         private Vector3 _buttonScale;
-        private Vector3 _iconScale;
         [SerializeField] private Image buttonImage = default;
-        [SerializeField] private Image buttonIcon = default;
 
         public void Init()
         {
             _buttonScale = buttonImage.transform.localScale;
-            _iconScale = buttonIcon.transform.localScale;
         }
 
         public void FadeIn(float animationTime = 0.0f)
@@ -25,12 +22,6 @@ namespace Kai.Common.Presentation.View
                     .SetEase(Ease.Linear))
                 .Join(buttonImage.rectTransform
                     .DOScale(_buttonScale * 0.3f, animationTime)
-                    .SetEase(Ease.OutQuart))
-                .Join(buttonIcon
-                    .DOFade(0.0f, animationTime)
-                    .SetEase(Ease.Linear))
-                .Join(buttonIcon.rectTransform
-                    .DOScale(_iconScale * 0.3f, animationTime)
                     .SetEase(Ease.OutQuart));
         }
 
@@ -42,12 +33,6 @@ namespace Kai.Common.Presentation.View
                     .SetEase(Ease.Linear))
                 .Join(buttonImage.rectTransform
                     .DOScale(_buttonScale, animationTime)
-                    .SetEase(Ease.OutBack))
-                .Join(buttonIcon
-                    .DOFade(1.0f, animationTime)
-                    .SetEase(Ease.Linear))
-                .Join(buttonIcon.rectTransform
-                    .DOScale(_iconScale, animationTime)
                     .SetEase(Ease.OutBack));
         }
     }
