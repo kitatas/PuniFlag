4241194a5703921d577cbac713dbde1d83726910
kitatas <315kitatas10703@gmail.com>
2023-09-06 22:54:20 +0900

    fix: 警告無効

--- Assets/Kai/Scripts/Game/Presentation/View/State/BaseState.cs
+++ Assets/Kai/Scripts/Game/Presentation/View/State/BaseState.cs
@@ -9,12 +9,16 @@ namespace Kai.Game.Presentation.View.State
     {
         public abstract GameState GetState();
 
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
         public virtual async UniTask InitAsync(CancellationToken token)
+#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
         {
 
         }
 
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
         public virtual async UniTask<GameState> TickAsync(CancellationToken token)
+#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
         {
             return GameState.None;
         }
