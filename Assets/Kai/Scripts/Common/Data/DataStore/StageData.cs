bad6f085c3d4de3bfa4da8621919696022d5a2fe
kitatas <315kitatas10703@gmail.com>
2023-10-27 00:08:12 +0900

    fix: Odinへの依存を削除

--- Assets/Kai/Scripts/Common/Data/DataStore/StageData.cs
+++ Assets/Kai/Scripts/Common/Data/DataStore/StageData.cs
@@ -1,4 +1,3 @@
-using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace Kai.Common.Data.DataStore
@@ -6,16 +5,8 @@ namespace Kai.Common.Data.DataStore
     [CreateAssetMenu(fileName = nameof(StageData), menuName = "DataTable/" + nameof(StageData), order = 0)]
     public sealed class StageData : ScriptableObject
     {
-#if UNITY_EDITOR
-        [PreviewField]
-#endif
         [SerializeField] private Sprite lockTexture = default;
-
-#if UNITY_EDITOR
-        [PreviewField]
-#endif
         [SerializeField] private Sprite clearTexture = default;
-
         [SerializeField] private TextAsset stageJsonData = default;
 
         public Sprite GetButtonTexture(bool isClear) => isClear ? clearTexture : lockTexture;
